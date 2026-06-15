6483b7452ae1fbe45cf5492a3a21f00596589e26
RonnyLP <2223010014@untels.edu.pe>
2026-06-15 06:29:47 -0500

    feat(usuario): agregar método create en el servicio de usuario

--- src/main/java/pe/edu/untels/certificadosdrsu/servicesimplements/UsuarioServiceImplement.java
+++ src/main/java/pe/edu/untels/certificadosdrsu/servicesimplements/UsuarioServiceImplement.java
@@ -1,9 +1,12 @@
 package pe.edu.untels.certificadosdrsu.servicesimplements;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import pe.edu.untels.certificadosdrsu.dtos.UserRequest;
 import pe.edu.untels.certificadosdrsu.entities.Usuario;
-import pe.edu.untels.certificadosdrsu.repositories.IUsuarioRepository;
+import pe.edu.untels.certificadosdrsu.repositories.UsuarioRepository;
 import pe.edu.untels.certificadosdrsu.servicesinterface.IUsuarioService;
 import java.util.List;
 import java.util.Optional;
@@ -11,11 +14,30 @@ import java.util.Optional;
 @Service
 public class UsuarioServiceImplement implements IUsuarioService {
   @Autowired
-  private IUsuarioRepository uR;
+  private UsuarioRepository uR;
+
+  @Autowired
+  private PasswordEncoder passwordEncoder;
 
   @Override
   public void insert(Usuario usuario) {
     uR.save(usuario);
+  }
+
+  @Override
+  public Usuario create(UserRequest request) {
+    if (uR.existsByUsername(request.getUsername())) {
+      throw new RuntimeException("El username ya existe");
+    }
+
+    Usuario usuario = new Usuario();
+    usuario.setUsername(request.getUsername());
+    usuario.setPasswordHash(passwordEncoder.encode(request.getPassword()));
+    usuario.setRol(request.getRol() != null ? request.getRol() : "USER");
+    usuario.setIdParticipante(request.getIdParticipante());
+    usuario.setActivo(true);
+
+    return uR.save(usuario);
   }
 
   @Override
